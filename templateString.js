f69812aef5f0770e059e1c543488b29fd3faaa68
Palak <palakagarwal1237@gmail.com>
2022-02-15 21:09:56 +0530

    fix: minor changes of syntax

--- templateString.js
+++ templateString.js
@@ -1,6 +1,9 @@
-const es6String=() => 
-`Hello, ${process.argv[2]}!
-Your name lowercased is "${process.argv[2].toLowerCase()}".`; 
+
+const es6String=() => {
+    return `Hello, ${process.argv[2]}!
+    Your name lowercased is "${process.argv[2].toLowerCase()}".`; 
+    }
 
 console.log (es6String());
 
+module.exports= {templateString:es6String};
